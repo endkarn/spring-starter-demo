2c0f89fbe9ebac7e383d6e11d99e1965f2acd549
Karnawat <endkarn0@gmail.com>
2019-01-09 13:43:50 +0700

    สร้าง User API

---
+++ src/main/java/com/example/demo/user/UserService.java
@@ -0,0 +1,7 @@
+package com.example.demo.user;
+
+import com.example.demo.dto.UserDto;
+
+public interface UserService {
+    UserDto getUserDto (Long id);
+}
---
+++ src/main/java/com/example/demo/user/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.demo.user;
+
+import com.example.demo.dto.UserDto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    List<UserDto> userDtoList = new ArrayList<>();
+
+
+    public UserServiceImpl() {
+        userDtoList.add(new UserDto(0L,"firstname","lastname"));
+        userDtoList.add(new UserDto(1L,"กานวัด","คนดี"));
+        userDtoList.add(new UserDto(2L,"น้องน้ำ","บ้านไกล"));
+        userDtoList.add(new UserDto(3L,"บักฟลุ้ค","สารคาม"));
+    }
+
+    public UserDto getUserDto(Long id){
+       return userDtoList.get(id.intValue());
+   }
+
+}
